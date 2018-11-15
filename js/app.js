65b410f5768a399f55dc5f557eb166d17f608aab
Alex Cho <alexsjcho@gmail.com>
2018-11-15 07:59:57 -0800

    finished app

--- js/app.js
+++ js/app.js
@@ -1,22 +1,52 @@
 document.querySelector('#generate-names').addEventListener('submit',loadNames);
 
-//Execute the function to query the API
-function loadNames(e){
-    e.preventDefault();
+    //Execute the function to query the API
+    function loadNames(e){
+        e.preventDefault();
 
-//Read the values from the form and create the variables
-const origin = document.getElementById('country').value;
-const genre = document.getElementById('genre').value;
-const amount = document.getElementById('quantity').value;
+    //Read the values from the form and create the variables
+    const origin = document.getElementById('country').value;
+    const genre = document.getElementById('genre').value;
+    const amount = document.getElementById('quantity').value;
 
-//Build the URL
-let url = 'http://uninames.com/api/?';
-//Read the origin and append to the url
-if(origin !== ''){
-    url += `region=${origin}&`;
+    //Build the URL
+    let url = 'http://uninames.com/api/?';
+    //Read the gender and append to the url
+    if(gender !== ''){
+        url += `gender=${gender}&`;
+    //Read the amount and append to the url
+    if(amount !== ''){
+        url += `amount=${amount}&`;
+    }
+
+    //Ajax Call
+    const xhr = new XMLHttpRequest();
+
+
+    //Open the connection
+    xhr.open('GET',url, true);
+
+    //Execute the function
+    xhr.onload = function(){
+        if(this.status === 200){
+            const names = JSON.parse(this.responseText);
+
+            //Insert into the HTML
+            let html = '<h2>Generated Names</h2>';
+            html += '<ul name"list">';
+            names.forEach(fucntion(name){
+                html += `
+                    <li> ${name.name} </li>
+                `;
+            });
+            html += '</ul>';
+
+            document.querySelector('#result').innerHTML = html;
+
+        }
+    }
+
+    //Send the request
+    xhr.send();
+
 }
-
-
-}
-
-
